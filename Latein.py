b1329e931931ae9ba5f5b7a0a4e39ec0f28414f3
Simon Thomas <simon.paul.thomas@gmx.de>
2023-03-25 12:22:53 +0100

    konjungtiv fuktionirt

--- Latein.py
+++ Latein.py
@@ -3,7 +3,7 @@ voc = {"laudare":["loben","preisen","anpriesen","gutheißen","rühmen","a"]}
 
 
 #frage = input("Frage: ")
-frage = "esse"
+frage = "laudare"
 
 # open a file
 file1 = open("latein_ausnahmen.json", "r")
@@ -15,7 +15,7 @@ ausnahmen = json.loads(ausnahmen_str)
 def übersetzung():
     for i in range(0,len(voc[frage])-1):
         print(voc[frage][i])
-def konjugieren(indikativ,aktiv,zeit,wort : str):
+def konjugieren(indikativ,aktiv,zeit,mänliche_form,wort : str):
     output = []
     if wort in ausnahmen:
         if indikativ:
@@ -29,42 +29,79 @@ def konjugieren(indikativ,aktiv,zeit,wort : str):
         for SingPl in ["Sing","Pl"]:
             for p in ["1.","2.","3."]:
                 output.append(ausnahmen[wort][indikativ_Konjungtiv][aktiv_passiv][zeit][SingPl][p])
-    else:
+
+    elif zeit == "präsens" or zeit == "imperfekt" or zeit == "perfekt" and aktiv or zeit == "futur" or zeit == "futur2" and aktiv or zeit == "plusquamperfekt" and aktiv:
         grundform = wort.rstrip("re")
-        if zeit == "plusquamperfekt":
-            grundform = grundform + "vera"
-        if zeit == "futur2":
-            grundform = grundform + "ver"
-        if zeit == "imperfekt":
-            grundform = grundform + "ba"
-        if zeit == "futur":
-            grundform = grundform + "b"
-            endungsart = "futur"
-        elif zeit == "perfekt":
-            grundform = grundform + "v"
-            endungsart = "perfekt"
-        elif aktiv:
+
+        if aktiv:
             endungsart = "aktiv"
         else:
             endungsart = "passiv"
+        if not indikativ:
+            endungsart = "aktiv_m"
+        if zeit == "präsens" and not indikativ:
+            grundform = grundform.rstrip("a")+"e"
+        if zeit == "plusquamperfekt":
+            endungsart = "aktiv_m"
+            if indikativ:
+                grundform = grundform + "vera"
+            else:
+                grundform = grundform + "visse"
+        if zeit == "futur2":
+            grundform = grundform + "ver"
+        if zeit == "imperfekt":
+            endungsart = "aktiv_m"
+            if indikativ:
+                grundform = grundform + "ba"
+            else:
+                grundform = grundform + "re"
+        if zeit == "futur":
+            grundform = grundform + "b"
+            if aktiv:
+                endungsart = "futur"
+            else:
+                endungsart = "passiv_futur"
+        if zeit == "perfekt":
+            if indikativ:
+                grundform = grundform + "v"
+                endungsart = "perfekt"
+            else:
+                grundform = grundform + "vre"
+        
         endungen = {"aktiv":{"Sing":{"1.":"o","2.":"s","3.":"t"},"Pl":{"1.":"mus","2.":"tis","3.":"nt"}},
+                    "aktiv_m":{"Sing":{"1.":"m","2.":"s","3.":"t"},"Pl":{"1.":"mus","2.":"tis","3.":"nt"}},
                     "passiv":{"Sing":{"1.":"r","2.":"ris","3.":"tur"},"Pl":{"1.":"mur","2.":"mini","3.":"ntur"}},
                     "perfekt":{"Sing":{"1.":"i","2.":"isti","3.":"it"},"Pl":{"1.":"imus","2.":"istis","3.":"erunt"}},
-                    "futur":{"Sing":{"1.":"o","2.":"is","3.":"it"},"Pl":{"1.":"imus","2.":"itis","3.":"unt"}}}
-        if indikativ:
-            if zeit == "präsens" or zeit == "imperfekt" or zeit == "perfekt" and aktiv or zeit == "futur" and aktiv or zeit == "futur2" and aktiv or zeit == "plusquamperfekt" and aktiv:
-                for SingPl in ["Sing","Pl"]:
-                    for p in ["1.","2.","3."]:
-                            if endungen[endungsart][SingPl][p] == "o" and zeit == "imperfekt" or endungen[endungsart][SingPl][p] == "o" and zeit == "plusquamperfekt":
-                                output.append(grundform+"m")
-                            elif grundform.endswith("a") and endungen[endungsart][SingPl][p] == "o" and zeit == "präsens":
-                                output.append(grundform.rstrip("a")+"o")
-                            else:
-                                output.append(grundform+endungen[endungsart][SingPl][p])
+                    "futur":{"Sing":{"1.":"o","2.":"is","3.":"it"},"Pl":{"1.":"imus","2.":"itis","3.":"unt"}},
+                    "passiv_futur":{"Sing":{"1.":"or","2.":"eris","3.":"itur"},"Pl":{"1.":"imus","2.":"itis","3.":"untur"}}}
+        for SingPl in ["Sing","Pl"]:
+            for p in ["1.","2.","3."]:
+                    if grundform.endswith("a") and endungen[endungsart][SingPl][p] == "o" and zeit == "präsens":
+                        output.append(grundform.rstrip("a")+"o")
+                    elif grundform.endswith("a") and endungen[endungsart][SingPl][p] == "r" and zeit == "präsens":
+                        output.append(grundform.rstrip("a")+"or")
+                    else:
+                        output.append(grundform+endungen[endungsart][SingPl][p])
+
+    else:
+        grundform = wort.rstrip("re")
+        if zeit == "perfekt":
+            esse = konjugieren(True,True,"präsens",True,"esse")
+        if zeit == "plusquamperfekt":
+            esse = konjugieren(True,True,"imperfekt",True,"esse")
+        if zeit == "futur2":
+            esse = konjugieren(True,True,"futur",True,"esse")
+        for SingPl in ["Sing","Pl"]:
+            for p in ["1.","2.","3."]:
+                if SingPl == "Sing":
+                    output.append(grundform+"tus "+esse[int(p.rstrip("."))-1])
+                if SingPl == "Pl":
+                    output.append(grundform+"ti "+esse[int(p.rstrip("."))+2])
+
             
     return output
     
 
-print(konjugieren(True,True,"präsens",frage))
+print(konjugieren(False,True,"plusquamperfekt",True,frage))
 
-assert konjugieren(True,True,"präsens","lauda") == ['laudo', 'laudas', 'laudat', 'laudamus', 'laudatis', 'laudant']
+assert konjugieren(True,True,"präsens",True,"lauda") == ['laudo', 'laudas', 'laudat', 'laudamus', 'laudatis', 'laudant']
